50cdb0d33eed86003d83f3461d90676588409ebd
Conor Sun <sunzhe@oudot.cn>
2019-05-06 21:33:41 +0800

    update 0.4

--- UserScript/Chang.en-us.to.zh-CN.js
+++ UserScript/Chang.en-us.to.zh-CN.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         切换文档语言至简体中文
 // @namespace    https://github.com/Sunhelter/LearningFile/blob/master/UserScript/Chang.en-us.to.zh-CN.js
-// @version      0.3
-// @description  将en-us英文页面切换至zh-CN简体中文
+// @version      0.4
+// @description  支持MSDN/MDN
 // @author       Sunhelter
 // @license      MIT
 // @date         2019-05-06
@@ -12,12 +12,23 @@
 // ==/UserScript==
 
 (function () {
-    if (window.location.href.indexOf("/en-us") > -1) {
-        var url = window.location.href.replace('en-us', 'zh-CN');
-        window.location.href = url;
+    var url = window.location.href;
+    var cnurl = window.location.href;
+
+    if (url.indexOf("/en-us") > -1) {
+        cnurl = url.replace('/en-us', '/zh-CN');
     }
-    else if (window.location.href.indexOf("/zh-tw") > -1) {
-        var url = window.location.href.replace('zh-tw', 'zh-CN');
-        window.location.href = url;
+    else if (url.indexOf("/en-US") > -1) {
+        cnurl = url.replace('/en-US', '/zh-CN');
+    }
+    else if (url.indexOf("/zh-tw") > -1) {
+        cnurl = url.replace('/zh-tw', '/zh-CN');
+    }
+    else if (url.indexOf("/zh-TW") > -1) {
+        cnurl = url.replace('/zh-TW', '/zh-CN');
+    }
+
+    if (cnurl != url) {
+        window.location.href = cnurl;
     }
 })();
