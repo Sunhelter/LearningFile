79ab7a74541442c23953ee2b892077e0ef05fe6d
Conor Sun <sunhelter@gmail.com>
2023-02-23 18:40:15 +0800

    排除部分页面

--- UserScript/Chang.en-us.to.zh-CN.js
+++ UserScript/Chang.en-us.to.zh-CN.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         切换文档语言至简体中文
 // @namespace    https://github.com/Sunhelter/LearningFile/blob/master/UserScript/Chang.en-us.to.zh-CN.js
-// @version      0.7
+// @version      0.8
 // @description  支持MSDN/MDN
 // @author       Sunhelter
 // @license      MIT
@@ -11,6 +11,7 @@
 // @match        *://support.microsoft.com/*
 // @match        *://azure.microsoft.com/*
 // @match        *://developer.mozilla.org/*
+// @exclude      *://learn.microsoft.com/en-us/answers/
 // @grant        none
 // ==/UserScript==
 
